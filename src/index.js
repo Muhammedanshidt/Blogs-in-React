4c02d7938ffe47efd8a9d64b4f20bd71cb9bec46
Mohammed Thasleem MK <155818870+Thasleem-MK@users.noreply.github.com>
2024-02-01 10:30:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './Day 4/Model/Hooks';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <AuthProvider>
   <React.StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
   </React.StrictMode>
-  </AuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
